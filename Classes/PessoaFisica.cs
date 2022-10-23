a4916174c67a0283112d6a1650d824af9846d2b2
Amanda <amandagabylopes23@gmail.com>
2022-10-23 16:07:43 -0300

    atualização 3

--- Classes/PessoaFisica.cs
+++ Classes/PessoaFisica.cs
@@ -10,12 +10,54 @@ namespace Back_End_ER02.Classes
 
         public override float CalcularImposto(float rendimento)
         {
-            throw new NotImplementedException();
+            if (rendimento<=1500)
+            {
+                return 0;
+            } else if (rendimento > 1500 &&rendimento <=3500)
+            {
+                float resultado = (rendimento/100) * 1.5f;
+                return resultado;
+            } else 
+            {
+                float resultado = (rendimento/100) * 5;
+                return resultado;
+            }
+   
         }
 
-        public bool ValidarDataNasc(DateTime dataNasc)
+        public bool ValidarDataNasc (DateTime dataNasc)
         {
-            throw new NotImplementedException();
+            DateTime dataAtual = DateTime.Today;
+            double idade = (dataAtual - dataNasc).TotalDays / 365;
+            Console.WriteLine(idade);
+
+            if(idade>=18)
+            {
+                return true;
+
+            } else
+            {
+                return false;
+            }
+            
+        }
+
+        public bool ValidarDataNasc (string dataNasc)
+        {
+            if (DateTime.TryParse(dataNasc, out DateTime dataConvert))
+            {
+                DateTime dataAtual = DateTime.Today;
+                double idade = (dataAtual - dataConvert).TotalDays/365;
+                Console.WriteLine(idade);
+
+                if(idade >= 18)
+                {
+                    return true;
+
+                } else return false;
+            }
+
+            return false;
         }
     }
 }
--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -9,12 +9,49 @@ namespace Back_End_ER02.Classes
 
         public override float CalcularImposto(float rendimento)
         {
-            throw new NotImplementedException();
+            if (rendimento <= 3000)
+            {
+                return rendimento * 0.03f;
+
+            } 
+            else if (rendimento > 3000 && rendimento <= 6000)
+            {
+                return rendimento * 0.05f;
+
+            } 
+            else if (rendimento > 6000 && rendimento <= 10000)
+            {
+                return rendimento * 0.07f;
+            }
+            else
+            {
+                return rendimento * 0.09f;
+            }
         }
 
         public bool ValidarCnpj(string cnpj)
         {
-            throw new NotImplementedException();
+            bool CNPJValidacao = Regex.IsMatch(cnpj, @"^(\d{14}) | (\d{2}.\d{3}.\d{3}/\d{4}-\d{2})$");
+
+            if (CNPJValidacao)
+            {
+                string subStringCnpj14 = cnpj.Substring(8, 4);
+
+                if (subStringCnpj14 == "0001")
+                {
+                    return true;
+
+                } 
+            }
+
+            string subStringCnpj18 = cnpj.Substring(11, 4);
+
+            if(subStringCnpj18 == "0001")
+            {
+                return true;
+
+            }
+            return false;
         }
     }
 }
